bd39b6b4d98b2e57e2140477c8f70994a45b1ef4
Roman <romanwrest@icloud.com>
2025-08-02 21:10:00 +0300

    Update effect.js

--- effect.js
+++ effect.js
@@ -165,34 +165,35 @@ $('document').ready(function(){
 	});
 	
 	$('#story').click(function(){
-		$(this).fadeOut('slow');
-		$('.cake').fadeOut('fast').promise().done(function(){
-			$('.message').fadeIn('slow');
-		});
-		
-		var i;
+    $(this).fadeOut('slow');
+    $('.cake').fadeOut('fast').promise().done(function(){
+        $('.message').fadeIn('slow');
+    });
 
-		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
+    var i;
 
-		});
-			// body...
-		}
-		
-		msgLoop(0);
-		
-	});
+    function msgLoop(i) {
+        $("p:nth-child(" + i + ")").fadeOut('slow').delay(800).promise().done(function(){
+            i = i + 1;
+            $("p:nth-child(" + i + ")").fadeIn('slow').delay(1000).promise().done(function(){
+                // Прокрутка к текущей строке текста с помощью jQuery
+                $('html, body').animate({
+                    scrollTop: $("p:nth-child(" + i + ")").offset().top - ($(window).height() / 2)
+                }, 500);
+                
+                if (i == 50) {
+                    $("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+                        $('.cake').fadeIn('fast');
+                    });
+                } else {
+                    msgLoop(i);
+                }
+            });
+        });
+    }
+
+    msgLoop(0);
+});
 });
 
 
